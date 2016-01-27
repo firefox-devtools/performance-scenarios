f6bc3bf76601315d52d2f5a7bf6862faa2ae0166
wbamberg <wbamberg@mozilla.com>
2016-01-27 13:45:47 -0800

    Update alloc.js
    Alloc using an object, and a little more structure.

--- js-allocs/scripts/alloc.js
+++ js-allocs/scripts/alloc.js
@@ -1,30 +1,55 @@
 var MIN_WORD_LENGTH = 2;
-var MAX_WORD_LENGTH = 12;
-var WORD_COUNT = 100000;
-var words = [];
+var MAX_WORD_LENGTH = 100;
+var ITERATION_COUNT = 10;
+var ITERATION_LENGTH = 5000;
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function MyStuff() {
+  this.thingCollections = [];
+  this.name = "my collections of things";
+
+  this.makeThingCollections = function() {
+    for (var i = 0; i < ITERATION_COUNT; i++) {
+      this.makeThingCollection();
+    }
+  };
+  
+  this.makeThingCollection = function() {
+    var thingCollection = [];
+    for (var i = 0; i < ITERATION_LENGTH; i++) {
+      var thing = this.makeNewThing();
+      thingCollection.push(thing);
+    }
+    this.thingCollections.push(thingCollection);
+  };
+  
+  this.makeNewThing = function() {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var chars = "abcdefghijklmnopqrstuvwxyz";
+    var wordLength = getRandomInt(MIN_WORD_LENGTH, MAX_WORD_LENGTH);
+    var word = "";
+    for (var j = 0; j < wordLength; j++) {
+      word += chars[getRandomInt(0, chars.length-1)];
+    }
+    return new RegExp(word);
+  };
+  
+  this.logThings = function() {
+    console.log(this.thingCollections);
+  };
+  
+  this.clearThings = function() {
+    this.thingCollections.length = 0;
+  };  
+  
 }
 
-function makeWord() {
-  var chars = "abcdefghijklmnopqrstuvwxyz";
-  var wordLength = getRandomInt(MIN_WORD_LENGTH, MAX_WORD_LENGTH);
-  var word = "";
-  for (var j = 0; j < wordLength; j++) {
-    word += chars[getRandomInt(0, chars.length-1)];
-  }
-
-  return word;
-}
-
-function makeWords() {
-  words.length = 0;
-  for (var i = 0; i < WORD_COUNT; i++) {
-    words.push(makeWord());
-  }
-  console.log(words);
-}
+var myStuff = new MyStuff();
 
 var makeThings = document.getElementById("make-things");
-makeThings.addEventListener("click", makeWords);
+makeThings.addEventListener("click", function () {
+  myStuff.makeThingCollections();
+  myStuff.logThings();
+});
